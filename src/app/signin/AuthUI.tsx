efba52a86a32c0c103dc7695014f5c0ae5936834
DevTK0 <tko.developer.02@gmail.com>
2024-01-31 11:04:33 +0800

    Fixed all login bugs.

--- src/app/signin/AuthUI.tsx
+++ src/app/signin/AuthUI.tsx
@@ -7,13 +7,12 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default function AuthUI() {
     const { client } = useAuth();
-    console.log(`${getURL()}/auth/callback`);
     return (
         <div className="flex flex-col space-y-4">
             <Auth
                 supabaseClient={client}
                 providers={["discord"]}
-                redirectTo={`${getURL()}/auth/callback`}
+                redirectTo={`${getURL()}auth/callback`}
                 appearance={{ theme: ThemeSupa }}
                 theme="dark"
             />
