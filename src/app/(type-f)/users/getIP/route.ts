3241fd7b09d57b2c696619051caa99faf8c06a0f
DevTK0 <tko.developer.02@gmail.com>
2024-02-04 10:57:40 +0800

    More code organisation.

--- src/app/(type-f)/users/getIP/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server";
-
-export async function GET(req: NextRequest) {
-    let ip = req.ip || req.headers.get("X-Forwarded-For");
-
-    return Response.json(ip);
-}
