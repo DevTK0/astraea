ce81efebb2feecb642e439eab04c8cae9e439cac
DevTK0 <tko.developer.02@gmail.com>
2024-04-15 00:18:59 +0800

    Test remove status display.

--- src/app/(typef)/games/[game]/servers/[serverId]/(components)/overview.tsx
+++ src/app/(typef)/games/[game]/servers/[serverId]/(components)/overview.tsx
@@ -4,7 +4,9 @@ export default function Overview() {
     return (
         <>
             <div className="flex flex-row items-start space-x-2">
-                <div className="w-full space-y-2">{/* <ServerStatus /> */}</div>
+                <div className="w-full space-y-2">
+                    <ServerStatus />
+                </div>
             </div>
         </>
     );
