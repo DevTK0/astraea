9b44e9084ff0d99dc3d2624a42574437112be09f
DevTK0 <tko.developer.02@gmail.com>
2024-04-15 23:34:06 +0800

    Added Astraea gems UI.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,6 +3,8 @@ import { MainNav } from "./main-nav";
 import { UserNav } from "./user-nav";
 import { getUser } from "@/lib/auth/server";
 import { redirect } from "next/navigation";
+import { Icons } from "../ui/icons";
+import { Button } from "../ui/button";
 
 export default async function Navbar() {
     const user = await getUser();
@@ -17,7 +19,17 @@ export default async function Navbar() {
                 <div className="flex h-16 items-center px-4">
                     <Logo />
                     <MainNav className="mx-6" />
-                    <div className="ml-auto flex items-center space-x-4">
+                    <div className="ml-auto flex items-center space-x-12">
+                        <div className="flex items-center">
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                className="rounded-full"
+                            >
+                                <Icons.sketch className="h-4 w-4 text-cyan-500" />
+                            </Button>
+                            <div className="text-cyan-500"> 500 </div>
+                        </div>
                         <UserNav user={user} />
                     </div>
                 </div>
