c1ff6d1707837b3ec616239cef4cab69338f34a5
DevTK0 <tko.developer.02@gmail.com>
2024-05-09 17:21:08 +0800

    Added redirects to existing routes.

--- src/app/(typef)/home/page.tsx
+++ src/app/(typef)/home/page.tsx
@@ -1,3 +1,7 @@
+import { routes } from "@/(global)/configs/site";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+    redirect(routes.landing);
     return <> Placeholder Homepage </>;
 }
