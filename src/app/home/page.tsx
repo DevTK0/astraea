1d421a7debcfad247c1af5acc0ed89bc914033f2
DevTK0 <tko.developer.02@gmail.com>
2024-05-28 13:38:45 +0800

    feat(blog): :alembic: Experiment with @next/mdx for the blog
    Add @next/mdx and dependencies and a basic mdx page for testing and experimentation.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,6 @@ import { routes } from "@/(global)/configs/site";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-    redirect(routes.landing);
+    // redirect(routes.landing);
     return <> Placeholder Homepage </>;
 }
